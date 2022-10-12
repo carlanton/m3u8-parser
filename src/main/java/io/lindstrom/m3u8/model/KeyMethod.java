75e404aea26ebcba3f83d1d71fe18d5d869dbe8e
Owen Roberts <roberts.7@icloud.com>
2022-10-12 16:48:46 -0600

    add SAMPLE-AES-CTR support

--- src/main/java/io/lindstrom/m3u8/model/KeyMethod.java
+++ src/main/java/io/lindstrom/m3u8/model/KeyMethod.java
@@ -1,7 +1,7 @@
 package io.lindstrom.m3u8.model;
 
 public enum KeyMethod {
-    NONE, AES_128, SAMPLE_AES;
+    NONE, AES_128, SAMPLE_AES, SAMPLE_AES_CTR;
 
     @Override
     public String toString() {
@@ -10,6 +10,8 @@ public enum KeyMethod {
                 return AES_128_STRING;
             case SAMPLE_AES:
                 return SAMPLE_AES_STRING;
+            case SAMPLE_AES_CTR:
+                return SAMPLE_AES_CTR_STRING;
             default:
                 return super.toString();
         }
@@ -21,6 +23,8 @@ public enum KeyMethod {
                 return AES_128;
             case SAMPLE_AES_STRING:
                 return SAMPLE_AES;
+            case SAMPLE_AES_CTR_STRING:
+                return SAMPLE_AES_CTR;
             default:
                 return KeyMethod.valueOf(name);
         }
@@ -28,4 +32,5 @@ public enum KeyMethod {
 
     private static final String AES_128_STRING = "AES-128";
     private static final String SAMPLE_AES_STRING = "SAMPLE-AES";
+    private static final String SAMPLE_AES_CTR_STRING = "SAMPLE-AES-CTR";
 }
