cbfe0246b3f54b50cd04f3761d260afcbf476ec6
Bhaskar Nidumukalla <bhaskar.nidumukalla@dena.jp>
2022-11-21 22:18:08 +0900

    fix: Write EXT-X-DEFINE tags to media playlist as well

--- src/test/java/io/lindstrom/m3u8/parser/MediaPlaylistReadAndWrite.java
+++ src/test/java/io/lindstrom/m3u8/parser/MediaPlaylistReadAndWrite.java
@@ -33,6 +33,7 @@ public class MediaPlaylistReadAndWrite {
                 "media/media-playlist-with-date-range-and-cue-out-cue-in.m3u8",
                 "media/issue-17.m3u8",
                 "media/ll-hls.m3u8",
+                "media/media-variables.m3u8",
                 "open-m3u8/liveMediaPlaylist.m3u8",
                 "open-m3u8/negativeDurationMediaPlaylist.m3u8",
                 "open-m3u8/playlistWithEncryptedMediaSegments.m3u8",
